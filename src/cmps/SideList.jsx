59624acd7450aeecf171713d2191dbb39404a033
nShahaf <shahafnoked@gmail.com>
2024-07-21 11:57:57 +0300

    set hover and acite state in side list

--- src/cmps/SideList.jsx
+++ src/cmps/SideList.jsx
@@ -1,17 +1,23 @@
-import {useNavigate } from "react-router-dom"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export function SideList({ stations }) {
     const navigate = useNavigate()
+    const [activeId, setActiveId] = useState(null)
 
     function handleClick(id, type) {
-       navigate(`/${type}/${id}`)
+        setActiveId(id)
+        // navigate(`/${type}/${id}`)
     }
 
     if (!stations) return null
     return (
         <ul className='side-list-container clean-list'>{
             stations.map(station => (
-                <article className="side-card" key={station.id} onClick={() => handleClick(station.id, station.type)}>
+                <article
+                    className={activeId === station.id ? 'side-card active' : 'side-card'}
+                    key={station.id}
+                    onClick={() => handleClick(station.id, station.type)}>
                     <div className="img-container square-ratio">
                         <img src={station.imgs[2].url} />
                     </div>

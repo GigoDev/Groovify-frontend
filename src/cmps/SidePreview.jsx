5540fa30816c2e3c1fc64ccdb146029c84db1c7f
nShahaf <shahafnoked@gmail.com>
2024-07-21 10:53:54 +0300

    Squashed commit of the following:
    commit 4036f913e5b08ac4209cb3d6f9c8699c8b06d919
    Author: Daniel Vinitsky <danielvinisky@gmail.com>
    Date:   Sun Jul 21 10:52:37 2024 +0300
    
        Add Player component svgs for readability

---
+++ src/cmps/SidePreview.jsx
@@ -0,0 +1,17 @@
+export function SidePreview() {
+    return (
+        <div className="side-preview-line">
+            <h1>Hi!</h1>
+            {/* <img className='side-preview-img' src={station.imgUrl} alt={station.name} />
+            <div className="station-preview-details">
+                <span className="station-name">{station.name}</span>
+                <span className='station-created'>{station.createdBy.fullname || 'Guest'}</span>
+                <span className='station-created'>Playlist
+                    <span className='station-created'>
+                        <span> • </span>
+                        {station.songs.length} songs</span>
+                </span>
+            </div> */}
+        </div>
+    )
+}

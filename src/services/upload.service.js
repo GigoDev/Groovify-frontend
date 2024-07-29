b14c376e02131fd0798f1deced439536ed5e32ef
nShahaf <shahafnoked@gmail.com>
2024-07-29 15:35:39 +0300

    station modal now create cloudinery url

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -2,24 +2,28 @@ export const uploadService = {
 	uploadImg,
 }
 
-async function uploadImg(ev) {
-	const CLOUD_NAME = 'dmbgmvobl'
-	const UPLOAD_PRESET = 'groovify-uploads'
+async function uploadImg(file) {
+	
+	const CLOUD_NAME = 'dzl5lalwt' //Cloud name
+	const UPLOAD_PRESET = 'groovify-preset' //preset settings
 	const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
-
 	const formData = new FormData()
 	
     // Building the request body
-	formData.append('file', ev.target.files[0])
+	formData.append('file', file)
 	formData.append('upload_preset', UPLOAD_PRESET)
 	
     // Sending a post method request to Cloudinary API
 	try {
-		const res = await fetch(UPLOAD_URL, { method: 'POST', body: formData })
-		const imgData = await res.json()
-		return imgData
+		const res = await fetch(UPLOAD_URL,{
+			method: 'POST', 
+			body: formData 
+		})
+		const { url } = await res.json()
+		return url
+
 	} catch (err) {
-		console.error(err)
+		console.error("Error uploading image to cloudinery", err)
 		throw err
 	}
 }
